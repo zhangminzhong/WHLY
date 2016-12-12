7469f5facf094d3a58b46d1afd1a1ebf07695b65
zhangminzhong <1065639989@qq.com>
2016-12-12 19:50:49 +0800

    删除 新建文本文档.txt 修改包名

--- meterManagement/meterManagement-SmsMeterLib/src/main/java/org/whut/meterManagement/smsmeterlib/frame/FrameDirection.java
+++ meterManagement/meterManagement-SmsMeterLib/src/main/java/org/whut/meterManagement/smsmeterlib/frame/FrameDirection.java
@@ -1,15 +1,15 @@
-package org.whut.meterManagement.smsmeterlib.smsprocess;
+package org.whut.meterManagement.smsmeterlib.frame;
 
 /**
  * Created with IntelliJ IDEA.
  * User: zhang_minzhong
  * Date: 16-12-12
- * Time: 上午10:22
+ * Time: 上午10:23
  * To change this template use File | Settings | File Templates.
  */
 /// <summary>
-/// 帧参数类型枚举
+/// 帧方向，REQUEST起始帧，REPLY应答帧
 /// </summary>
-public enum ParamType {
-    INT, STR, FLT, BTA
+public enum FrameDirection {
+    REQUEST, REPLY
 }
--- meterManagement/meterManagement-SmsMeterLib/src/main/java/org/whut/meterManagement/smsmeterlib/frame/ParamType.java
+++ meterManagement/meterManagement-SmsMeterLib/src/main/java/org/whut/meterManagement/smsmeterlib/frame/ParamType.java
@@ -1,15 +1,15 @@
-package org.whut.meterManagement.smsmeterlib.smsprocess;
+package org.whut.meterManagement.smsmeterlib.frame;
 
 /**
  * Created with IntelliJ IDEA.
  * User: zhang_minzhong
  * Date: 16-12-12
- * Time: 上午10:24
+ * Time: 上午10:22
  * To change this template use File | Settings | File Templates.
  */
 /// <summary>
-/// 应答帧执行结果，SUCCESS成功，FAIL失败
+/// 帧参数类型枚举
 /// </summary>
-public enum FrameResult {
-    SUCCESS,FAIL
+public enum ParamType {
+    INT, STR, FLT, BTA
 }
